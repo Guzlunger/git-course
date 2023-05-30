75ffd3537671cbdcfa66700980cb2e2c3c4f09e0
Guzlunger <sh.rabadanov23@gmail.com>
2023-05-30 20:45:56 +0300

    change App.test.tsx - 2

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -8,4 +8,15 @@ test('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
 
